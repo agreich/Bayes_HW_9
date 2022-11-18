4b6a397644b3da3b9af788f6884f97a1df0555ad
Alexandra Reich <agreich@alaska.edu>
2022-11-18 14:01:22 -0900

    Moving up in the JAGS world. Both models fit successfully

--- Bayes_HW_9_script.R
+++ Bayes_HW_9_script.R
@@ -24,13 +24,12 @@ AIC(mod1, mod2)
 #2a
 #use JAGS to fit Model 2
 library(rjags)
-
-#use JAGS to fit Model 1
-num.betas <- 3
+num.betas <- 4
 beta.prior.mean <- rep(0.0, num.betas) # p. 217 code
-beta.prior.precision <- 1/20
+beta.prior.var <- 10^5 *diag(num.betas)
+beta.prior.precision <- solve(beta.prior.var)
 sigma.min <- 0
-sigma.max <- 10^5
+sigma.max <- 20
 my.data <- list(
   y=mtcars$mpg, cwt = mtcars$cwt, 
   cqsec = mtcars$cqsec,
@@ -40,14 +39,43 @@ my.data <- list(
   sigma.min = sigma.min, sigma.max=sigma.max
 )
 
-my.inits = list(beta=rep(0.0, num.betas), sigma=1.0)#,
- # list(beta=rep(0.0, num.betas), sigma=1.0),
-  #list(beta=rep(0.0, num.betas), sigma=1.0)
-#)
-
-my.fname <- "mod_statement_hw9_1"
-my.jags.mod1 <- jags.model(
-  file=my.fname, data=my.data, inits= my.inits,
-  n.adapt=1000, quiet=FALSE, n.chains=1
+my.inits = list(list(beta=rep(0.0, num.betas), sigma=10),
+                list(beta=rep(0.0, num.betas), sigma=10),
+                list(beta=rep(0.0, num.betas), sigma=10)
 )
 
+my.fname_2 <- "mod_statement_hw9_2"
+my.jags.mod2 <- jags.model(
+  file=my.fname_2, data=my.data, inits= my.inits,
+  n.adapt=1000, quiet=FALSE, n.chains=3
+)
+
+
+#use JAGS to fit Model 1
+num.betas <- 3
+beta.prior.mean <- rep(0.0, num.betas) # p. 217 code
+beta.prior.var <- 10^5 *diag(num.betas)
+beta.prior.precision <- solve(beta.prior.var)
+sigma.min <- 0
+sigma.max <- 20
+my.data <- list(
+  y=mtcars$mpg, cwt = mtcars$cwt, 
+  cqsec = mtcars$cqsec,
+  N=nrow(mtcars),
+  beta.prior.mean = beta.prior.mean,
+  beta.prior.precision = beta.prior.precision,
+  sigma.min = sigma.min, sigma.max=sigma.max
+)
+
+my.inits = list(list(beta=rep(0.0, num.betas), sigma=10),
+  list(beta=rep(0.0, num.betas), sigma=10),
+  list(beta=rep(0.0, num.betas), sigma=10)
+)
+
+my.fname_1 <- "mod_statement_hw9_1"
+my.jags.mod1 <- jags.model(
+  file=my.fname, data=my.data, inits= my.inits,
+  n.adapt=1000, quiet=FALSE, n.chains=3
+)
+
+dic.samples(my.fname_1, n.iter=10000, thin=1, type="pD")
